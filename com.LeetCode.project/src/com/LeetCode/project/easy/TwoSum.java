0b1ec4aa0049e3f03f5d461ebfe8649d3f8d438d
05946N744 <05946N744@9.102.29.181>
2021-05-26 18:09:49 +0530

    draft.

--- com.LeetCode.project/src/com/LeetCode/project/easy/TwoSum.java
+++ com.LeetCode.project/src/com/LeetCode/project/easy/TwoSum.java
@@ -2,7 +2,7 @@ package com.LeetCode.project.easy;
 
 import java.util.HashMap;
 import java.util.Map;
-
+ 
 /**
  * Given an array of integers nums and an integer target, return indices of the
  * two numbers such that they add up to target.
